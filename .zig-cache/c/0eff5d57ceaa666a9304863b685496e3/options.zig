dcce7c8efe38a6fbd10ab7381056e6869d7a1185
Eyad Ahmed Ragheb <KernelState2012@gmail.com>
2026-03-14 16:29:15 +0200

    feat: no chache

--- .zig-cache/c/0eff5d57ceaa666a9304863b685496e3/options.zig
+++
@@ -1,6 +0,0 @@
-pub const callbacks: ?bool = null;
-pub const version: @import("std").SemanticVersion = .{
-    .major = 3,
-    .minor = 0,
-    .patch = 0,
-};
--- .zig-cache/c/e38f700fb26a8dd8e1ee1748bc4cf888/options.zig
+++
@@ -1,4 +0,0 @@
-pub const libc: bool = true;
-pub const tiny_file_dialogs: bool = true;
-pub const freetype: bool = true;
-pub const tree_sitter: bool = true;
